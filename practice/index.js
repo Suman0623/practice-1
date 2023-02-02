e78e9ce56de6eb4bf77e9ce76358e4d80b1393e9
Suman0623 <baddikasumanth23@gmail.com>
2023-02-02 12:49:35 +0530

    okie

--- practice/index.js
+++ practice/index.js
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom'
 import App from './App'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
 //ReactDOM.render(<div><h1> Hello Good Morning</h1><h2>Hello Good Afternoon</h2></div>, document.getElementById("abc"))
 ReactDOM.render(<App/>, document.getElementById('abc'))
---
+++ react-3/src/App.jsx
@@ -0,0 +1,8 @@
+import Navbar from "./Navbar"
+let App =() =>{
+    return <div>
+        <Navbar/>
+        <h1>Good Morning</h1>
+    </div>
+}
+export default App
